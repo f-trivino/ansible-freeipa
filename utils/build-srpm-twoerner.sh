2a05bb9617216d38e65b12c94308443b95c5ef0e
Francisco Trivino <ftrivino@redhat.com>
2020-08-20 12:22:56 +0200

    Add .copr Makefile

--- utils/build-srpm-twoerner.sh
+++ utils/build-srpm-twoerner.sh
@@ -10,10 +10,6 @@ topdir=$(dirname $0)
 
 sed -e "s/@@VERSION@@/$version/g" -e "s/@@RELEASE@@/$release/g" -e "s/@@DATE@@/$date/g" $topdir/ansible-freeipa.spec.in > ansible-freeipa.spec
 
-git add ansible-freeipa.spec
-git commit -m "SPECFILE" ansible-freeipa.spec
 git archive --format=tar --prefix=ansible-freeipa-${version}-${release}/ 'HEAD' | bzip2 -c > ansible-freeipa-${version}-${release}.tar.bz2
-git reset --hard HEAD~1
 
-
-rpmbuild -ts ansible-freeipa-${version}-${release}.tar.bz2
+rpmbuild --define "_sourcedir $PWD" -bs ansible-freeipa.spec
